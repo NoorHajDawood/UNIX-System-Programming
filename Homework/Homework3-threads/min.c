0eeef893fea298f7951717a066ce3193685de934
NoorHajDawood <noorhajdawood@gmail.com>
2021-08-22 16:36:22 +0300

    comments & arr type fix

--- Homework/Homework3-threads/min.c
+++ Homework/Homework3-threads/min.c
@@ -5,7 +5,7 @@
 typedef struct _array
 {
     int length;
-    int *array;
+    long *array;
 } array;
 
 void *findMin(void *arg)
@@ -24,44 +24,49 @@ int main(int argc, const char *argv[])
 {
     pthread_t tid1, tid2;
     void *ret;
-    long min;
+    long min, *arr;
     int length = argc - 1;
-    int *arr;
     array arr1 = {length / 2 + length % 2, arr};
-    array arr2 = {length - length / 2 - length % 2, arr + length / 2 + length % 2};
+    array arr2 = {length - arr1.length, arr + arr1.length};
 
-
+    // didnt get an array in the program's arguments
     if (argc <= 1)
     {
         fprintf(stderr, "expected array values\n");
         return 1;
     }
 
-    arr = (int *)malloc(length * sizeof(int));
-    if(arr == NULL)
+    // allocating the array to parse from char* to long
+    arr = (long *)malloc(length * sizeof(long));
+    if (arr == NULL)
     {
         perror("malloc");
         return 2;
     }
-    
+    // parsing from char* to long
     for (int i = 1; i < argc; i++)
     {
         arr[i - 1] = atol(argv[i]);
     }
 
+    // create a thrid to find min in the first half
     if (pthread_create(&tid1, NULL, findMin, &arr1))
     {
         perror("pthread_create(1)");
         return 1;
     }
 
-    if (argc > 2)
+    // create a thrid to find min in the 2nd half
+    if (argc > 2) // make sure there's a 2nd half
+    {
         if (pthread_create(&tid2, NULL, findMin, &arr2))
         {
             perror("pthread_create(2)");
             return 1;
         }
+    }
 
+    // wait for 1st thread to return a value
     if (pthread_join(tid1, &ret))
     {
         fprintf(stderr, "Could not join tid1 Thread\n");
@@ -70,7 +75,8 @@ int main(int argc, const char *argv[])
 
     min = (long)ret;
 
-    if (argc > 2)
+    // wait for 2nd thread to return a value
+    if (argc > 2) // if there's a 2nd thread
     {
         if (pthread_join(tid2, &ret))
         {
@@ -82,6 +88,5 @@ int main(int argc, const char *argv[])
     }
 
     printf("Min = [%ld]\n", min);
-
     return 0;
 }
